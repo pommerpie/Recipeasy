3f400c3a273431cb3423b84b807a419e12a602ed
Andrew Ross <andrew.ross4224@gmail.com>
2023-11-30 19:32:19 -0500

    added route for getting recipies by ingredient, this route hits the spoonacular api

--- routes/ingredient.js
+++ routes/ingredient.js
@@ -1,6 +1,11 @@
 const ingredient = require('express').Router();
 
-ingredient.get('/', (req, res) =>{
-    res.json('found me')
+ingredient.get('/', async (req, res) =>{
+    const ingredients = 'chicken,+milk,+cheese,+bacon'
+    const url = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=077448ed5ae54a95a34829c3e60eee0f&ingredients=${ingredients}&ignorePantry=true`
+    const response = await fetch(url)
+    const data = await response.json()
+    res.json(data)
+    
 })
 module.exports = ingredient;
