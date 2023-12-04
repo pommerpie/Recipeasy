55540f5e959119efbaee7001a81907caf909ac26
Andrew Ross <andrew.ross4224@gmail.com>
2023-12-04 18:34:19 -0500

    updating api routes for spoonacular

--- routes/ingredient.js
+++ routes/ingredient.js
@@ -1,11 +1,13 @@
-const ingredient = require('express').Router();
+const byingredient = require('express').Router();
 
-ingredient.get('/', async (req, res) =>{
-    const ingredients = 'chicken,+milk,+cheese,+bacon'
-    const url = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=077448ed5ae54a95a34829c3e60eee0f&ingredients=${ingredients}&ignorePantry=true`
-    const response = await fetch(url)
-    const data = await response.json()
-    res.json(data)
-    
+byingredient.get('/', async (req, res) => {
+    const ingredients = ['chicken','milk','cheese','butter']
+    for (let index in ingredients) {
+        ingredients[index] = ingredients[index] + ',+'
+    }
+    const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=077448ed5ae54a95a34829c3e60eee0f&includeIngredients=milk,cheese,butter,chicken,onions,salt,pepper&fillIngredients=true`
+    const response = await fetch(url);
+    const data = await response.json();
+    res.json(data);
 })
-module.exports = ingredient;
+module.exports = byingredient;
