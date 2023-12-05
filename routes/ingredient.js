e1bb24e280a28d9579330cd3b4530f33f07f0e16
Andrew Ross <andrew.ross4224@gmail.com>
2023-12-04 20:03:30 -0500

    added urls for api fetch requests

--- routes/ingredient.js
+++ routes/ingredient.js
@@ -1,13 +1,13 @@
 const byingredient = require('express').Router();
 
 byingredient.get('/', async (req, res) => {
-    const ingredients = ['chicken','milk','cheese','butter']
-    for (let index in ingredients) {
-        ingredients[index] = ingredients[index] + ',+'
-    }
-    const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=077448ed5ae54a95a34829c3e60eee0f&includeIngredients=milk,cheese,butter,chicken,onions,salt,pepper&fillIngredients=true`
+    const ingredients = ['milk','cheese','butter',5062]
+    const url = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=077448ed5ae54a95a34829c3e60eee0f&ingredients=${ingredients}&ranking=1&ignorePantry=true`
     const response = await fetch(url);
     const data = await response.json();
     res.json(data);
 })
 module.exports = byingredient;
+
+// const url2 = `https://api.spoonacular.com/recipes/${recipeId}/analyzedInstructions?apiKey=077448ed5ae54a95a34829c3e60eee0f`
+// const url3 = `https://api.spoonacular.com/recipes/${recipeId}/summary`
