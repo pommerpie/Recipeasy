bff240c31043663c5d027058cedf2e7aab7c56fc
Chris.Openshaw <copenshaw17@gmail.com>
2023-12-04 19:07:32 -0600

    added user registration function

--- db/register.js
+++ db/register.js
@@ -1,2 +1,6 @@
-import { sequelize } from '../config/connection.js';
+import { Users } from './models.js';
 
+async function registerNewUser(name, password) {
+    let newUser = await Users.create({ firstName: `${name}`, password: `${password}` });
+    console.log(`${name}'s auto-generated ID:`, newUser.id);
+}
