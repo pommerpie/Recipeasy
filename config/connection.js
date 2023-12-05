016c106b6bb0689cc2a02b4c22e5774da4ceb0f8
Chris.Openshaw <copenshaw17@gmail.com>
2023-12-04 20:01:40 -0600

    setting up my env

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,15 @@
-import { Users, Ingredients, Recipes } from '../models/users.js';
-
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306,
-      models: [Users, Recipes, Ingredients]
-    }
-  );
-}
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306,
+  }
+);
 
 module.exports = sequelize;
