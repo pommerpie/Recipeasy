69fea4b890010a1226a9ef944353ff15383a7061
Chris.Openshaw <copenshaw17@gmail.com>
2023-12-06 19:49:36 -0600

    fixed exports

--- db/queries.js
+++ db/queries.js
@@ -1,14 +1,20 @@
-const Users  = require("../models/users")
+const { Users, Recipes }  = require("../models")
 const sequelize = require('../config/connection');
 
-export async function insertUser(email, name, password) {
+async function insertUser(email, name, password) {
     const user = await Users.create({ email: `${email}`, firstName: `${name}`, password: `${password}` });
     console.log("User's auto-generated ID:", user.id);
 }
 
-export async function showUsers() {
+async function showUsers() {
     const users = await Users.findAll();
     console.log(users.every(user => user instanceof Users)); // true
     console.log("All users:", JSON.stringify(users, null, 2));
 }
 
+insertUser("chris@gmail.com", "chris", "kjdbfkjhfbwe")
+
+module.exports = {
+    insertUser,
+    showUsers
+}
