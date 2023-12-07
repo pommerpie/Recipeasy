d2e2affb3749f22dc1dfb2b1f5d1cd7f0b8ab27b
Andrew Ross <andrew.ross4224@gmail.com>
2023-12-06 19:36:44 -0500

    added route under api for user login checking input vs database

---
+++ routes/api/user.js
@@ -0,0 +1,47 @@
+const router = require('express').Router();
+const { Users } = require('../../models')
+
+router.get('/login', async (req, res) => {
+    try {
+        const dbUserData = await Users.findOne({
+          where: {
+            email: req.body.email,
+          },
+        });
+    
+        if (!dbUserData) {
+          res
+            .status(400)
+            .json({ message: 'Incorrect email or password. Please try again!' });
+          return;
+        }
+    
+        const validPassword = await dbUserData.checkPassword(req.body.password);
+    
+        if (!validPassword) {
+          res
+            .status(400)
+            .json({ message: 'Incorrect email or password. Please try again!' });
+          return;
+        }
+    
+        req.session.save(() => {
+          req.session.loggedIn = true;
+          console.log(
+            'File: user-routes.js ~ line 57 ~ req.session.save ~ req.session.cookie',
+            req.session.cookie
+          );
+    
+          res
+            .status(200)
+            .json({ user: dbUserData, message: 'You are now logged in!' });
+        });
+      } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+      }
+})
+
+
+
+module.exports = router;
