bdb4d43074197c43d65ff91af4446b899b5a9c02
Andrew Ross <andrew.ross4224@gmail.com>
2023-12-07 19:56:40 -0500

    added login data so it can be seen by handlebars

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
 
 router.get('/', async (req, res) => {
-    res.render('landing');
+    res.render('landing', {loggedIn: req.session.loggedIn});
 });
 
 router.get('/login', async (req, res) => {
-    res.render('login');
+    res.render('login', {loggedIn: req.session.loggedIn});
 });
 
 router.get('/signup', async (req, res) => {
-    res.render('signup');
+    res.render('signup', {loggedIn: req.session.loggedIn});
 });
 
 module.exports = router;
