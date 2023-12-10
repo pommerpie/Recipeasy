7b798fc02c45ca88e63d01c51957c97e1c4e0d56
Andrew Ross <andrew.ross4224@gmail.com>
2023-12-10 10:03:53 -0500

    changed signup so you are automatically logged in after signing up

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,23 +1,28 @@
 const signupForm = async (event) => {
-    event.preventDefault();
-  
-    const email = document.getElementById('email').value.trim();
-    const firstName = document.getElementById('first').value.trim();
-    const password = document.getElementById('pass').value.trim();
-  
-    if (email && firstName && password) {
-      const response = await fetch('/api/user/signup', {
+  event.preventDefault();
+
+  const email = document.getElementById('email').value.trim();
+  const firstName = document.getElementById('first').value.trim();
+  const password = document.getElementById('pass').value.trim();
+
+  if (email && firstName && password) {
+    const response = await fetch('/api/user/signup', {
+      method: 'POST',
+      body: JSON.stringify({ email, firstName, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      const response = await fetch('/api/user/login', {
         method: 'POST',
-        body: JSON.stringify({ email, firstName, password }),
+        body: JSON.stringify({ email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert('Failed to sign up.');
-      }
+      document.location.replace('/');
+    } else {
+      alert('Failed to sign up.');
     }
-  };
+  }
+};
 
-  document.getElementById('form').addEventListener('submit', signupForm);
+document.getElementById('form').addEventListener('submit', signupForm);
