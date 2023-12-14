e4c47fb0c941acfbf5474bd8d549fe9a37efd9df
Andrew Ross <andrew.ross4224@gmail.com>
2023-12-14 11:27:47 -0500

    finished single recipe render

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
 
-const spoonacular = require('./spoonacular');
 const user = require('./user')
 
-router.use('/spoonacular', spoonacular);
 router.use('/user', user);
 
 module.exports = router;
--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,22 +1,22 @@
 const router = require('express').Router();
 const { Ingredidents } = require('../models')
-
+// Landing page
 router.get('/', async (req, res) => {
     res.render('landing', { loggedIn: req.session.loggedIn });
 });
-
+// Login page
 router.get('/login', async (req, res) => {
     res.render('login', { loggedIn: req.session.loggedIn });
 });
-
+// Signup page
 router.get('/signup', async (req, res) => {
     res.render('signup', { loggedIn: req.session.loggedIn });
 });
-
+// User saved recipes
 router.get('/myrecipes', async (req, res) => {
     res.render('recipe', { loggedIn: req.session.loggedIn })
 })
-
+// Loading search page with ingredients in database
 router.get('/search', async (req, res) => {
     try {
         const dbingredients = await Ingredidents.findAll({
@@ -37,9 +37,7 @@ router.get('/search', async (req, res) => {
         res.status(500).json(err);
     }
 })
-router.get('/recipes', async (req,res) => {
-    
-})
+// Search by selected ingredients and render result page
 router.get('/byIngredient/:ingredients', async (req, res) => {
     let ingredients = req.params.ingredients
     const url = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${process.env.API_KEY}&ingredients=${ingredients}&ranking=1&ignorePantry=true&number=5`
@@ -47,4 +45,17 @@ router.get('/byIngredient/:ingredients', async (req, res) => {
     const data = await response.json();
     res.render('recipe', { loggedIn: req.session.loggedIn, data: data, style: "../css/search.css", script: "../js/recipe.js" });
 })
+// getting recipe instuctions based on id
+router.get('/instructions/:recipeId', async (req, res) => {
+    const url =`https://api.spoonacular.com/recipes/${req.params.recipeId}/summary?apiKey=${process.env.API_KEY}`
+    const url2 = `https://api.spoonacular.com/recipes/${req.params.recipeId}/analyzedInstructions?apiKey=${process.env.API_KEY}`
+    const response = await fetch(url);
+    const summary = await response.json();
+    const response2 = await fetch(url2);
+    const data = await response2.json();
+    const instructions = data[0].steps
+    console.log(instructions)
+    res.render('singleRecipe', { loggedIn: req.session.loggedIn, summary, instructions, style: "../css/search.css", script: "" });
+})
+
 module.exports = router;
